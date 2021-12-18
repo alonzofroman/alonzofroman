0fd7c954a0836ab3b89ff9e0e3526d3e158dea8b
Roman <alonzofroman@gmail.com>
2021-12-18 10:13:32 -0800

    React router dom working

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
      <Router>
        <Navbar />
        <Routes>
-         <Route path="/about" component={About} />
-         <Route path='/contact' component={Contact} />
-         
+         <Route path="/about" element={<About />} />
+         <Route path='/contact' element={<Contact />} />
+
        </Routes>
      </Router> 
   );
