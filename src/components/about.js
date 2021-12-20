ef47a230a84886a1b1dcf3364820ee9cf09253fc
alonzofroman <alonzofroman@gmail.com>
2021-12-20 15:33:57 -0800

    Added skills section to about me

--- src/components/about.js
+++ src/components/about.js
@@ -1,23 +1,84 @@
-import React from 'react';
-import '../styles/about.css';
+import React from "react";
+import "../styles/about.css";
+import { FaJsSquare, FaGithub, FaGitAlt, FaHtml5, FaCss3, FaNodeJs, FaReact } from "react-icons/fa";
+import { DiMysql, DiMongodb } from 'react-icons/di';
+import { SiApollographql } from 'react-icons/si';
 
-function About () {
-    return (
-        <div className='aboutMe'>
-            <h2 className='aboutTitle'>About Me</h2>
+function About() {
+  return (
+    <div className='aboutMe'>
+      <h2 className='aboutTitle'>About Me</h2>
 
-            <p className="aboutText">My name is Alonzo Roman, full-stack web developer and graduate of
-             UC Berkeley Extension's Coding Bootcamp.
-            The technical skills that I've gained from the coding bootcamp include HTML,
-            Cascading Style Sheets (CSS), Javascript, jQuery, Node.js, Express,
-            MySQL, Sequelize, Git, React, MongoDB, and REST APIs. My experiences in the coding
-            bootcamp have prepared me and made me confident in my ability to work in
-            any team environment, and my experience in the Marine Corps has given me a strong work ethic.</p>
-        
-            <h3>Skills</h3>
+      <p className='aboutText'>
+        My name is Alonzo Roman, full-stack web developer and graduate of UC
+        Berkeley Extension's Coding Bootcamp. The technical skills that I've
+        gained from the coding bootcamp include HTML, Cascading Style Sheets
+        (CSS), Javascript, jQuery, Node.js, Express, MySQL, Sequelize, Git,
+        React, MongoDB, and REST APIs. My experiences in the coding bootcamp
+        have prepared me and made me confident in my ability to work in any team
+        environment, and my experience in the Marine Corps has given me a strong
+        work ethic.
+      </p>
 
+      <h3>Skills</h3>
+      <div className='skillIcons'>
+
+      <div className='skill'>
+            <p>HTML</p>
+            <FaHtml5 size={60} />
         </div>
-    )
+
+        <div className='skill'>
+          <p>Javascript</p>
+          <FaJsSquare size={60} />
+        </div>
+
+        
+
+        <div className='skill'>
+            <p>CSS</p>
+            <FaCss3 size={60} />
+        </div>
+
+        <div className='skill'>
+            <p>NodeJS</p>
+            <FaNodeJs size={60} />
+        </div>
+
+        <div className='skill'>
+            <p>mySQL</p>
+            <DiMysql size={60} />
+        </div>
+
+        <div className='skill'>
+            <p>MongoDB</p>
+            <DiMongodb size={60} />
+        </div>
+
+        <div className='skill'>
+            <p>React</p>
+            <FaReact size={60} />
+        </div>
+
+        <div className='skill'>
+            <p>Apollo</p>
+            <SiApollographql size={60} />
+        </div>
+
+        <div className='skill'>
+            <p>Git</p>
+            <FaGitAlt size={60} />
+            
+        </div>
+
+        <div className='skill'>
+            <p>Github</p>
+            <FaGithub size={60} />
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default About;
