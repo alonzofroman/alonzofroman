55dbc3b3f897ffb7e5a30aa2a089a70f914c059c
Roman <alonzofroman@gmail.com>
2021-12-22 14:54:40 -0800

    Added pure carousel for projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
-function Projects () {
-    return (
-        <div className='projectpage'>
-            <h2>Projects</h2>
-        </div>
-    )
-};
+function Projects() {
+  return (
+    <div className='projectpage'>
+      <h2>Projects</h2>
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={125}
+        totalSlides={3}
+        infinite={true}
+      >
+        <Slider>
+          <Slide index={0}>I am the first Slide.</Slide>
+          <Slide index={1}>I am the second Slide.</Slide>
+          <Slide index={2}>I am the third Slide.</Slide>
+        </Slider>
+        <ButtonBack>Back</ButtonBack>
+        <ButtonNext>Next</ButtonNext>
+      </CarouselProvider>
+    </div>
+  );
+}
 
 export default Projects;
