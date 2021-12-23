2a027f5002933f6365dc339a07950d01e1df0a83
Roman <alonzofroman@gmail.com>
2021-12-23 13:36:39 -0800

    Added links to project carousel

--- src/components/about.js
+++ src/components/about.js
@@ -11,10 +11,7 @@ function About() {
 
       <p className='aboutText'>
         My name is Alonzo Roman, full-stack web developer and graduate of UC
-        Berkeley Extension's Coding Bootcamp. The technical skills that I've
-        gained from the coding bootcamp include HTML, Cascading Style Sheets
-        (CSS), Javascript, jQuery, Node.js, Express, MySQL, Sequelize, Git,
-        React, MongoDB, and REST APIs. My experiences in the coding bootcamp
+        Berkeley Extension's Coding Bootcamp. My experiences in the coding bootcamp
         have prepared me and made me confident in my ability to work in any team
         environment, and my experience in the Marine Corps has given me a strong
         work ethic.
@@ -32,8 +29,6 @@ function About() {
           <p>Javascript</p>
           <FaJsSquare size={60} />
         </div>
-
-        
 
         <div className='skill'>
             <p>CSS</p>
--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { FaGithub } from 'react-icons/fa';
+import { CgWebsite } from 'react-icons/cg';
 import { Carousel } from 'react-responsive-carousel';
+import '../styles/carousel.css';
 import Charity from '../assets/images/charities.jpg';
 import Haunted from '../assets/images/haunted.jpg';
 import Weather from '../assets/images/weather.jpg';
 
+
+
 export default class ProjectCarousel extends Component {
     render() {
         return (
-            <Carousel infiniteLoop={true} >
+            <Carousel className='carousel' infiniteLoop={true} showIndicators={false} >
                 <div>
-                    <img src={Charity} alt='charity site'/>
+                    <img className='carImg' src={Charity} alt='charity site'/>
                     <p className='legend'>Charity Site</p>
+                    <div className='carLinks'>
+                    <a href='https://github.com/alonzofroman/charities-project'><FaGithub size={30}/></a>
+                    <a href='https://alonzofroman.github.io/charities-project/'><CgWebsite size={30}/></a>
+                    </div>
                 </div>
 
                 <div>
-                    <img src={Haunted} alt='haunted site'/>
-                    <p className='legend'>Haunted Site</p>
+                    <img className='carImg' src={Haunted} alt='haunted site'/>
+                     <p className='legend'>Haunted Site</p>
+                     <div className='carLinks'>
+                         <a href='https://github.com/alonzofroman/haunted-forum'><FaGithub size={30}/></a>
+                         <a href='https://warm-springs-18820.herokuapp.com/'><CgWebsite size={30} /></a>
+                     </div>
                 </div>
 
                 <div>
-                    <img src={Weather} alt='weather site'/>
+                    <img className='carImg' src={Weather} alt='weather site'/>
                     <p className='legend'>Weather Site</p>
+                    <div className='carLinks'>
+                        <a href='https://github.com/alonzofroman/weather-dash'><FaGithub size={30} /></a>
+                        <a href='https://alonzofroman.github.io/weather-dash/'><CgWebsite size={30}/></a>
+                    </div>
                 </div>
 
             </Carousel>
