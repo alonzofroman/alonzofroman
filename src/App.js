a20e320da641087892b430eb642de7693c220754
Roman <alonzofroman@gmail.com>
2021-12-27 13:20:35 -0800

    Fixed routing issue with homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
-import About from './components/about';
-import Contact from './components/contact';
-import Projects from './components/projects';
-import Footer from './components/footer';
-import Home from './components/home';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/navbar";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Projects from "./components/projects";
+import Footer from "./components/footer";
+import Home from "./components/home";
 
 function App() {
   return (
-     <Router>
-       <Navbar />
-       <Routes>
-         <Route path="/about" element={<About />} />
-         <Route path='/contact' element={<Contact />} />
-         <Route path='/projects' element={<Projects />} />
-         <Route path='/' element={<Home />} />
-       </Routes>
-       <Footer />
-     </Router> 
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path={process.env.PUBLIC_URL + ""} element={<Navigate to="/" />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
